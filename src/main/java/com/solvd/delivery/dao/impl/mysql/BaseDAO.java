975509093164cd5b4f71d14156a6b699ff6fd588
jslozanopenagos <jslozanopenagos@laba.solvd.com>
2025-10-05 12:06:48 -0500

    addition of ConnectionPool & BaseDao classes

---
+++ src/main/java/com/solvd/delivery/dao/impl/mysql/BaseDAO.java
@@ -0,0 +1,21 @@
+package com.solvd.delivery.dao.impl.mysql;
+
+import com.solvd.delivery.util.ConnectionPool;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public abstract class BaseDAO {
+
+    protected Connection getConnection() throws SQLException {
+        return ConnectionPool.getInstance().getConnection();
+    }
+
+    protected void releaseConnection() {
+        try {
+            ConnectionPool.getInstance().releaseConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/solvd/delivery/util/ConnectionPool.java
+++ src/main/java/com/solvd/delivery/util/ConnectionPool.java
@@ -1,4 +1,46 @@
 package com.solvd.delivery.util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
 public class ConnectionPool {
+    private static ConnectionPool instance;
+    private Connection connection;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/food_delivery_app_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private ConnectionPool() throws SQLException {
+        try {
+            Class.forName(DRIVER);
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found", e);
+        }
+    }
+
+    public static synchronized ConnectionPool getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new ConnectionPool();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void releaseConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
